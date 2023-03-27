815cd2e21c6dcdb83b648eb08bfcbb2c2e09c6fa
Route9Library <you@example.com>
2023-03-27 17:45:38 -0400

    completed 3.2

--- src/main/java/com/codedifferently/lab/partA/Chickens01.java
+++ src/main/java/com/codedifferently/lab/partA/Chickens01.java
@@ -3,5 +3,21 @@ package com.codedifferently.lab.partA;
 public class Chickens01 {
     //Put your code here
 
-    //System.out.println(totalEggs);
+    public static void main(String[] args) {
+
+        int chickenCount = 8;
+        int eggsPerChicken = 4;
+        int totalEggs;
+
+        //Monday
+        System.out.println("On Monday Chicken Count is " + chickenCount);
+        //Tuesday
+        chickenCount = chickenCount + 1;
+        System.out.println("On Tuesday Chicken Count is " + chickenCount);
+        //Wednesday
+        chickenCount = chickenCount / 2;
+        System.out.println("On Wednesday Chicken Count is " + chickenCount);
+        totalEggs = chickenCount * eggsPerChicken;
+        System.out.println("Total Eggs Are " + totalEggs);
+    }
 }
--- src/main/java/com/codedifferently/lab/partA/Chickens02.java
+++ src/main/java/com/codedifferently/lab/partA/Chickens02.java
@@ -1,11 +1,30 @@
 package com.codedifferently.lab.partA;
 
 public class Chickens02 {
-    public static void main(String[] args) {
+
         //Put your code here
 
-        //System.out.println("Daily Average:   " +dailyAverage);
-        //System.out.println("Monthly Average: " +monthlyAverage);
-        //System.out.println("Monthly Profit:  $" +monthlyProfit);
+        public static void main(String[] args) {
+
+            int eggsCollected = 100;
+
+            //Monday
+            System.out.println("On Monday Eggs Collected are " + eggsCollected);
+            //Tuesday
+            eggsCollected = eggsCollected + 121;
+            System.out.println("On Tuesday Eggs Collected are " + eggsCollected);
+            //Wednesday
+            eggsCollected = eggsCollected + 117;
+            System.out.println("On Wednesday Eggs Collected are " + eggsCollected);
+            //calculate daily average
+            double dailyAverage = eggsCollected / 3.0;
+            //Calculate 30 day average
+            System.out.println("The 3 day average is " + dailyAverage);
+            double monthlyAverage = dailyAverage * 30;
+            System.out.println("The Monthly Average is " + monthlyAverage);
+            //Calculate Total Profit
+            double profitPerEgg = 0.18;
+            double monthlyProfit = monthlyAverage * profitPerEgg;
+            System.out.println("Estimated Total Profit is " + monthlyProfit);
+        }
     }
-}
